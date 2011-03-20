309663ce3f5283dff71e5395d86849588eb637a8
Tomas Lucovic <tomaslucovic@gmail.com>
2011-03-20 13:40:03 +0100

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def title
      base_title = "Ruby on Rails Tutorial Sample App"
      if @title.nil?
-       base_titel
+       base_title
      else
        "#{base_title} | #{@title}"
      end
---
+++ s
@@ -0,0 +1,5 @@
+Script started on Sun Mar 20 12:16:59 2011
+bash-3.2$ rail[K[K[K[Kexit
+exit
+
+Script done on Sun Mar 20 12:17:04 2011

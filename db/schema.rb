a405b512d01af8471d78639385f1edf90dd28500
Tomas Lucovic <tomaslucovic@gmail.com>
2011-04-11 23:05:38 +0200

    Done with user edit/update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110327130619) do
+ActiveRecord::Schema.define(:version => 20110411111458) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20110327130619) do
     t.datetime "updated_at"
     t.string   "encrypted_password"
     t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
 
 end
